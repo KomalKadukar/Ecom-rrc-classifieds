50f9d1eb602a68fd4f5ca63442436806c1910ba0
KomalKadukar <kkadukar@gmail.com>
2018-11-04 15:48:51 -0600

    Modified category reference in classified resource file.

--- app/admin/classifieds.rb
+++ app/admin/classifieds.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Classified do
-  permit_params :item, :price, :description, :category, :student_id
+  permit_params :item, :price, :description, :category_id, :student_id, :admin_ad, :sold
 end
