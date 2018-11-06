42e78732560dbe987c9d3596ac88bf8401da587d
KomalKadukar <kkadukar@gmail.com>
2018-11-06 14:48:04 -0600

    Added controller for Category.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :classified, only: [:index, :show]
+  resources :category, only: [:index, :show]
   resources :pages, only: [:show]
 
   get ':permalink', to: 'pages#permalink', as: 'permalink'
