0b7c19e338d2ba167515d0a4e388177ba8e9d9b6
KomalKadukar <kkadukar@gmail.com>
2018-11-06 15:10:40 -0600

    Added alias for categories route.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :classified, only: [:index, :show]
-  resources :category, only: [:index, :show]
+  resources :category, only: [:index, :show], as: 'categories'
   resources :pages, only: [:show]
 
   get ':permalink', to: 'pages#permalink', as: 'permalink'
