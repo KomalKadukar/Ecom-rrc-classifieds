dc933e5f28d24ee1a3f023295603676aafd67e59
KomalKadukar <kkadukar@gmail.com>
2018-11-06 21:38:31 -0600

    Fixed rubocop offence by adding frozen string comment.

--- app/models/order_classified.rb
+++ app/models/order_classified.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OrderClassified < ApplicationRecord
   belongs_to :order
   belongs_to :classified
--- app/models/program_classified.rb
+++ app/models/program_classified.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ProgramClassified < ApplicationRecord
   belongs_to :program
   belongs_to :classified
