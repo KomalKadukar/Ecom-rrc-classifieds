dc1a81d9f8045856784049de2bb9493e06f1390e
KomalKadukar <kkadukar@gmail.com>
2018-11-08 22:14:04 -0600

    Added helper method to show programs in drop down list.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  private
+
+  def programs
+    @programs = Program.all
+  end
+
+  helper_method :programs
+
 end
