0a9de976029a957d6fd66bf48d69898bfdcc5955
KomalKadukar <kkadukar@gmail.com>
2018-11-08 22:13:31 -0600

    Modified route for program and added idex and show actions.

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,7 +1,9 @@
 class ProgramController < ApplicationController
   def index
+    @programs = Program.all
   end
 
   def show
+    @program = Program.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'program/index'
-  get 'program/show'
-  get 'category/index'
-  get 'category/show'
+  # get 'program/index'
+  # get 'program/show'
+  # get 'category/index'
+  # get 'category/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :classified, only: [:index, :show]
   resources :category, only: [:index, :show], as: 'categories'
+  resources :program, only: [:index, :show], as: 'programs'
   resources :pages, only: [:show]
 
   get ':permalink', to: 'pages#permalink', as: 'permalink'
