aef05f25c98c1e5cbacd8baebc4e621678f8c61e
KomalKadukar <kkadukar@gmail.com>
2018-11-09 23:39:06 -0600

    Modified classified controller to add results method.

--- app/controllers/classified_controller.rb
+++ app/controllers/classified_controller.rb
@@ -8,4 +8,20 @@ class ClassifiedController < ApplicationController
   def show
     @classified = Classified.find(params[:id])
   end
+
+  def results
+    key = "%#{params[:query]}%"
+    selected_category = "#{params[:category]}"
+
+    if selected_category == 'all'
+      @classified_results =
+        Classified.where('item LIKE :search OR description LIKE :search', search: key)
+    else
+      @classified_results =
+        Classified.joins(:category).where('name = ?', selected_category)
+        .where('item LIKE :search OR description LIKE :search', search: key)
+    end
+
+
+  end
 end
