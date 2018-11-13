1ac1ed484a08e5ab57a5ccb040f378771967ba00
KomalKadukar <kkadukar@gmail.com>
2018-11-12 23:58:35 -0600

    Modified application controller to permit extra attributes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :number, :address, :city, :province_id])
+  end
+
   private
 
   def programs
