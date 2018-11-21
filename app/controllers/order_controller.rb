609fb02f941016277f1323435c371013697fb76e
KomalKadukar <kkadukar@gmail.com>
2018-11-21 10:22:20 -0600

    Added order summary view on checkout link.

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,6 +2,8 @@
 
 class OrderController < ApplicationController
   before_action :authenticate_student!
+  before_action :finalize_taxes
+  before_action :rate_type
 
   def order_summary
     @current_user = current_student
@@ -20,5 +22,26 @@ class OrderController < ApplicationController
       OrderClassified.create(order: @current_order, classified: classified,
                              item_price: classified.price)
     end
+  end
+
+  private
+
+  def finalize_taxes
+    @sub_total = @cart_classifieds.collect { |item| item.valid? ? item.price : 0 }.sum
+
+    @taxrate = current_student.province.province_rate +
+               current_student.province.canada_rate
+
+    @total_tax = @sub_total * @taxrate
+
+    @grand_total = @sub_total + @total_tax
+  end
+
+  def rate_type
+    @rates = {}
+    current_student.province.rate_type.split('+').map { |rt|
+      @rates[rt] =
+        rt == 'GST' ? current_student.province.canada_rate : current_student.province.province_rate
+    }
   end
 end
