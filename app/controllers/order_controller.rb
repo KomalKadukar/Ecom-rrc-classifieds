3ac7baab7821d8666745f998b5c6edeaf2009007
KomalKadukar <kkadukar@gmail.com>
2018-11-24 23:49:52 -0600

    Added new and create actions for payment processing.

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,26 +5,55 @@ class OrderController < ApplicationController
   before_action :finalize_taxes
   before_action :rate_type
 
-  def order_summary
+  def new
     @current_user = current_student
     @province = current_student.province
   end
 
+  def create
+    # Amount in cents
+    @amount = (@grand_total * 100).to_i
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    @charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'cad'
+    )
+
+    if @charge.paid && @charge.amount == @amount
+      create_order
+      session[:cart_items] = []
+    end
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_order_path
+  end
+
+  private
+
   def create_order
     @current_order = Order.create(
+      order_amount: @sub_total,
       student: current_student,
       province_rate: current_student.province.province_rate,
       canada_rate: current_student.province.canada_rate,
-      status: Status.find_by(name: 'Pending')
+      status: Status.find_by(name: 'Paid')
     )
 
     @cart_classifieds.each do |classified|
       OrderClassified.create(order: @current_order, classified: classified,
                              item_price: classified.price)
+
+      classified.update(sold: true)
     end
   end
-
-  private
 
   def finalize_taxes
     @sub_total = @cart_classifieds.collect { |item| item.valid? ? item.price : 0 }.sum
