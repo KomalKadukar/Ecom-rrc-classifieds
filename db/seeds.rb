0e7558b8775db50be5cc811f4da29c63394c8298
KomalKadukar <kkadukar@gmail.com>
2018-11-27 18:18:47 -0600

    Modified seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,73 +2,76 @@ require 'csv'
 
 province_csv = Rails.root.join('lib', 'seeds', 'Provinces_Taxes.csv')
 
-ProgramClassified.destroy_all
-OrderClassified.destroy_all
-Order.destroy_all
-Classified.destroy_all
-Student.destroy_all
-Province.destroy_all
-Category.destroy_all
-Program.destroy_all
-Status.destroy_all
+AdminUser.create!(email: 'komalkadukar@ecomrrc.com', password: 'password!', password_confirmation: 'password!')
+puts "There are now #{AdminUser.count} rows in the AdminUser table."
 
-CSV.foreach(province_csv, headers: true) do |row|
-  # puts row.to_hash
-  Province.create(name: row['name'],
-                  code: row['code'],
-                  rate_type: row['rate_type'],
-                  province_rate: row['province_rate'],
-                  canada_rate: row['canada_rate'])
-end
+# ProgramClassified.destroy_all
+# OrderClassified.destroy_all
+# Order.destroy_all
+# Classified.destroy_all
+# Student.destroy_all
+# Province.destroy_all
+# Category.destroy_all
+# Program.destroy_all
+# Status.destroy_all
 
-Category.create([{ name: 'Books' },
-                  { name: 'Cables' }])
-Program.create([{ name: "Business Information Technology"},
-                { name: "Business Technology Management"}])
-Status.create(name: 'Pending')
+# CSV.foreach(province_csv, headers: true) do |row|
+#   # puts row.to_hash
+#   Province.create(name: row['name'],
+#                   code: row['code'],
+#                   rate_type: row['rate_type'],
+#                   province_rate: row['province_rate'],
+#                   canada_rate: row['canada_rate'])
+# end
 
-2.times do
-  first_name = Faker::Name.unique.first_name
-  last_name = Faker::Name.unique.last_name
+# Category.create([{ name: 'Books' },
+#                   { name: 'Cables' }])
+# Program.create([{ name: "Business Information Technology"},
+#                 { name: "Business Technology Management"}])
+# Status.create(name: 'Pending')
 
-  Student.create(
-    name: "#{first_name} #{last_name}",
-    number: Faker::Number.unique.number(7),
-    email: "#{first_name.downcase}.#{last_name.downcase}@academic.rrc.ca",
-    address: Faker::Address.unique.street_address,
-    city: 'Calgary',
-    province: Province.first
-  )
-end
+# 2.times do
+#   first_name = Faker::Name.unique.first_name
+#   last_name = Faker::Name.unique.last_name
 
-Classified.create(
-  item: 'Mathematics Book',
-  price: 30.25,
-  description: 'Required for 1st term in BIT.',
-  category: Category.first,
-  student: Student.first,
-  admin_ad: false,
-  sold: false
-)
+#   Student.create(
+#     name: "#{first_name} #{last_name}",
+#     number: Faker::Number.unique.number(7),
+#     email: "#{first_name.downcase}.#{last_name.downcase}@academic.rrc.ca",
+#     address: Faker::Address.unique.street_address,
+#     city: 'Calgary',
+#     province: Province.first
+#   )
+# end
 
-Classified.create(
-  item: 'USB to VGA convertor cable',
-  price: 15,
-  description: 'Required in 2nd term for networking course.',
-  category: Category.second,
-  student: Student.second,
-  admin_ad: false,
-  sold: false
-)
+# Classified.create(
+#   item: 'Mathematics Book',
+#   price: 30.25,
+#   description: 'Required for 1st term in BIT.',
+#   category: Category.first,
+#   student: Student.first,
+#   admin_ad: false,
+#   sold: false
+# )
 
-ProgramClassified.create(program: Program.first, classified: Classified.first)
+# Classified.create(
+#   item: 'USB to VGA convertor cable',
+#   price: 15,
+#   description: 'Required in 2nd term for networking course.',
+#   category: Category.second,
+#   student: Student.second,
+#   admin_ad: false,
+#   sold: false
+# )
 
-puts "There are now #{Province.count} rows in the provinces table."
-puts "There are now #{Category.count} rows in the categories table."
-puts "There are now #{Program.count} rows in the programs table."
-puts "There are now #{Status.count} rows in the status table."
-puts "There are now #{Student.count} rows in the students table."
-puts "There are now #{Classified.count} rows in the classifieds table."
-puts "There are now #{ProgramClassified.count} rows in the program_classifieds table."
+# ProgramClassified.create(program: Program.first, classified: Classified.first)
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# puts "There are now #{Province.count} rows in the provinces table."
+# puts "There are now #{Category.count} rows in the categories table."
+# puts "There are now #{Program.count} rows in the programs table."
+# puts "There are now #{Status.count} rows in the status table."
+# puts "There are now #{Student.count} rows in the students table."
+# puts "There are now #{Classified.count} rows in the classifieds table."
+# puts "There are now #{ProgramClassified.count} rows in the program_classifieds table."
+
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
